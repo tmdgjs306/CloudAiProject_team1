8538df8fa4bb9a454b354663e225d14562232059
devRyan9025 <alex7006@naver.com>
2024-06-07 10:09:33 +0900

    feat: SecurityConfig 설정
    1. rememberMe 토큰 설정 -> 로그인 정보 유지

--- src/main/java/com/ryan9025/dog_dictionary/config/SecurityConfig.java
+++ src/main/java/com/ryan9025/dog_dictionary/config/SecurityConfig.java
@@ -1,22 +1,28 @@
 package com.ryan9025.dog_dictionary.config;
 
 import com.ryan9025.dog_dictionary.handler.CustomLoginFailureHandler;
+import com.ryan9025.dog_dictionary.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.UUID;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
     private final CustomLoginFailureHandler customLoginFailureHandler;
+    private final CustomUserDetailsService customUserDetailsService;
     /* 1. Security 이용해 로그인/로그아웃 기능 구현
      * 2. 기본적인 setting 및 csrf.disable 설정
      * 3. CustomLoginFailureHandler 추가
-     * 4. 최종 수정일: 2024.06.05 */
+     * 4. rememberMe 토큰 설정 -> 로그인 정보 유지
+     * 5. 최종 수정일: 2024.06.07 */
     @Bean
     public SecurityFilterChain filterChain (HttpSecurity httpSecurity) throws Exception {
         httpSecurity
@@ -31,9 +37,20 @@ public class SecurityConfig {
                         .usernameParameter("userId")
                         .passwordParameter("password")
                         .loginProcessingUrl("/auth/login")
-                        .failureHandler(customLoginFailureHandler)
+                        .failureHandler(customLoginFailureHandler) // 로그인 실패 핸들러
                         .defaultSuccessUrl("/board/feed",true)
                         .permitAll())
+                .logout((logout) -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"))
+                        .logoutSuccessUrl("/auth/login")
+                        .invalidateHttpSession(true) // 로그아웃 할 때, 세션 종료
+                        .deleteCookies("JSESSIONID") // 로그아웃 할 때. JSESSIONID 쿠키 제거
+                        .clearAuthentication(true)) // 로그아웃 할 때, 권한 제거
+                .rememberMe((auth) -> auth
+                        .rememberMeParameter("rememberUser") // 토큰명
+                        .key(UUID.randomUUID().toString()) // 토큰 식별 KEY
+                        .userDetailsService(customUserDetailsService) // 토큰에 저장 할 로그인 정보
+                        .tokenValiditySeconds(60*60*24)) // 토큰 유지시간 24H으로 설정
                 .csrf((csrf) -> csrf.disable());
         return httpSecurity.build();
 
